e62006d4f1aad7395549be728d93382ce7d96a9d
pradeep <levakupradeepkumar@gmail.com>
2025-05-21 11:38:03 +0530

    updated

---
+++ src/test/java/javaproblems/Fibonanci.java
@@ -0,0 +1,36 @@
+package javaproblems;
+
+import java.util.Scanner;
+
+public class Fibonanci {
+
+	public static void main(String[] args) {
+		
+		System.out.println("enter number : ");
+		Scanner input = new Scanner(System.in);
+		int num = 10;
+		
+		
+		int a = 0;
+		int b = 1;
+		
+		System.out.println(a);
+		
+//		for(int i=0;i<num;i++) {
+//			System.out.println(b);
+//			int temp = a+b;
+//			a=b;
+//			b=temp;
+//		}
+		
+		for(int i=0; i<num;i++) {
+			System.out.println(b);
+
+			int temp = b;
+			b =a+temp;
+			a = temp;
+			
+
+		}
+	}
+}
